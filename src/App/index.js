ae8ed8da3deaca01a5abfd0662ab0f6e62a963fc
nikhil kumar <nikhilkumar2092@gmail.com>
2020-05-22 23:56:30 +0530

    Added Styled Componants

--- src/App/index.js
+++ src/App/index.js
@@ -1,9 +1,7 @@
-import React from 'react';
-import './App.css';
-import WelcomeMessage from './componants/WelcomeMessage';
-import styled, {css} from 'styled-components';
-
-
+import React from "react";
+import "./App.css";
+import WelcomeMessage from "./componants/WelcomeMessage";
+import styled, { css } from "styled-components";
 
 function App() {
   return (
